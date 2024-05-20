d17cc3cbbc2f1ac05ed320a73aad556c8d6af8fe
ayntk-ai <122374094+ayntk-ai@users.noreply.github.com>
2024-05-20 20:33:32 +0200

    Revert "healthcheck_port"
    This reverts commit 0dfd1ed59cf142c1fb70b580fc32a1e1845564ab.

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5173, // Ensure this port is set
-  },
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-});
-
-
+})
